4959163c49647e8e3664143439096e5d793a9a26
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2022-01-12 23:34:03 +0100

    Dropdown menu

--- src/components/header/menuHeader.tsx
+++ src/components/header/menuHeader.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from '../../context/hooks'
 import { useTranslation } from 'react-i18next'
 import { LinkButton } from '../linkButton/linkButton'
 import circle from '../../assets/img/account_circle.svg'
+import arrow from '../../assets/img/arrow.svg'
 
 interface Props {}
 
@@ -23,8 +24,26 @@ export const MenuHeader: FC<Props> = (props) => {
                     <LinkButton label={t('NAV_DOCUMENTS')} path={''} />
                 </li>
                 <li className='container-user'>
-                    <img src={circle} alt='User account' />
-                    {user.user}
+                    <div className='dropdown-user'>
+                        <img src={circle} alt='User account' />
+                        {user.user}
+                        <img
+                            src={arrow}
+                            alt='expand menu'
+                            className='arrow-expand'
+                        />
+                    </div>
+                    <ul className='submenu'>
+                        <li>
+                            <LinkButton label={t('NAV_MY_ACCOUNT')} path={''} />
+                        </li>
+                        <li>
+                            <LinkButton
+                                label={t('NAV_LOGOUT')}
+                                onClick={() => console.log('logout')}
+                            />
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>
