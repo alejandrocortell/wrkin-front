8b48b2ef973921097e60a9289e3e931c1d0cff41
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2022-01-16 10:38:38 +0100

    Containers white

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export const Wrapper: FC<Props> = (props) => {
     return (
-        <div className='background-app'>
+        <>
             <Header />
-            {props.children}
-        </div>
+            <div className='background-app'>{props.children}</div>
+        </>
     )
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,23 +3,27 @@ import { useAppDispatch, useAppSelector } from '../../context/hooks'
 import { useTranslation } from 'react-i18next'
 import i18next from 'i18next'
 import { Wrapper } from '../../components/wrapper/wrapper'
+import { ContainerWhite } from '../../components/containerWhite/containerWhite'
 
 export const Home: FC = () => {
     const { t } = useTranslation()
     const user = useAppSelector((state) => state.user)
     const dispatch = useAppDispatch()
 
-    // const upId = () => {
-    //     dispatch(increment())
-    // }
-
-    // const downId = () => {
-    //     dispatch(decrement())
-    // }
-
     return (
         <Wrapper>
-            <div>Home page</div>
+            <section className='home container'>
+                <div className='column-timer'>
+                    <ContainerWhite>
+                        <div>Timeer</div>
+                    </ContainerWhite>
+                </div>
+                <div className='column-punchin'>
+                    <ContainerWhite>
+                        <div>Punchinss</div>
+                    </ContainerWhite>
+                </div>
+            </section>
         </Wrapper>
     )
 }
